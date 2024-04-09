9ce641b47013171e76a5c8257389383ac229c032
jjakob10 <johann.bastos@edu.ufes.br>
2024-04-09 14:12:37 -0300

    feat: imprimindo string vinda do site

--- src/main/java/br/ufes/inf/pi1/App.java
+++ src/main/java/br/ufes/inf/pi1/App.java
@@ -4,10 +4,9 @@ package br.ufes.inf.pi1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        PrevisaoDoTempo p = new PrevisaoDoTempo();
+        p.consolePrint();
     }
 }
---
+++ src/main/java/br/ufes/inf/pi1/PrevisaoDoTempo.java
@@ -0,0 +1,39 @@
+package br.ufes.inf.pi1;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class PrevisaoDoTempo {
+  public PrevisaoDoTempo() {
+
+  }
+
+  public void consolePrint() throws Exception {
+
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    HttpGet httpGet = new HttpGet(
+        "https://api.open-meteo.com/v1/forecast?latitude=-20.27&longitude=-40.31&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo");
+    CloseableHttpResponse response1 = httpclient.execute(httpGet);
+
+    // System.out.println(response1.getStatusLine());
+    HttpEntity entity1 = response1.getEntity();
+    // do something useful with the response body
+    // and ensure it is fully consumed
+    System.out.println(EntityUtils.toString(entity1));
+
+    // Parse string -> json
+    // JsonObject json =
+    // JsonParser.parseString(EntityUtils.toString(entity1)).getAsJsonObject();
+    // json.get("temperature");
+
+    response1.close();
+
+  }
+}
